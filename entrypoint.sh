0dae466bab5839b1cf23dca3db31bc63669ebd73
Radu Ailenei <railenei@gmail.com>
2025-03-10 17:31:44 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 
 # Load environment variables
 source /app/setup_env.sh
+source /app/setup_git.sh
 
 # Run the modular scripts
-/app/setup_git.sh
 /app/fetch_repos.sh
 /app/sync_workflows.sh
 /app/create_pr.sh
